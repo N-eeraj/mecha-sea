50c65fe59eec55dbe11a5da7e680002f713d4363
N-eeraj <neeraj7rajeena@gmail.com>
2023-09-06 13:11:35 +0530

    initial commit

---
+++ scripts/game.js
@@ -0,0 +1,35 @@
+import { Game } from './classes.js'
+
+// init method
+const init = () => {
+
+  // constants & variables
+  const GAME_WIDTH = 720
+  const GAME_HEIGHT = 480
+  const canvas = document.getElementById('canvas')
+  const ctx = canvas.getContext('2d')
+  
+  // game initialization
+  const game = new Game({
+    width: GAME_WIDTH,
+    height: GAME_HEIGHT
+  })
+
+  let lastTime = 0
+  const animate = (timeStamp) => {
+    const deltaTime = timeStamp - lastTime
+    lastTime = timeStamp
+
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT)
+    game.update()
+    game.draw(ctx)
+    requestAnimationFrame(animate)
+  }
+
+  canvas.width = GAME_WIDTH
+  canvas.height = GAME_HEIGHT
+
+  animate()
+}
+
+window.addEventListener('load', init)
